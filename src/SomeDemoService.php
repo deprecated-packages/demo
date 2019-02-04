d892d8dbbd80b9a6976ee3bcd22e56b615ec63e5
Tomas Votruba <tomas.vot@gmail.com>
2019-02-04 04:53:18 +0100

    prepare demo for php and code-quality sets

--- src/SomeDemoService.php
+++ src/SomeDemoService.php
@@ -1,11 +1,43 @@
 <?php declare(strict_types=1);
 
+// @todo use for gifs
+// --level code-quality
+// --level php70
+// --level php71
+// --level php74
+
 namespace App;
 
 final class SomeDemoService
 {
+    // @todo should be autocomplete by php74
+    private $value;
+
     public function someMethod()
     {
-        return 123;
+        $this->value = 'abc';
+
+        $value = 125;
+
+        return $value;
+    }
+
+    public function getName()
+    {
+        return 'Tom' . " & " . 'Jerry';
+    }
+
+    public function getDefalutValue($value)
+    {
+        return $value === null ? 10 : $value;
+    }
+
+    public function isCourseFull(?array $visitors)
+    {
+        if (count($visitors)) {
+            return true;
+        }
+
+        return false;
     }
 }
