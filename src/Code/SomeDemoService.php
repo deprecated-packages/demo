1532a2e0e124e7cdb59170208367b10750a16bb8
Tomas Votruba <tomas.vot@gmail.com>
2019-02-06 17:50:50 +0100

    step 2

--- src/Code/SomeDemoService.php
+++ src/Code/SomeDemoService.php
@@ -33,4 +33,26 @@ final class SomeDemoService
 
         return false;
     }
+
+    public function tooComplexArrayVerification(array $items, string $name)
+    {
+        foreach ($items as $item) {
+            if ($item === $name) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function isNullOrValue($oldToNewFunctions, string $currentFunction)
+    {
+        foreach ($oldToNewFunctions as $oldFunction => $newFunction) {
+            if ($currentFunction === $oldFunction) {
+                return $newFunction;
+                }
+            }
+
+        return null;
+    }
 }
