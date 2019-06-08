f8336b7265902024cab8defb0e15aff5dd87f32c
Tomas Votruba <tomas.vot@gmail.com>
2019-06-08 05:34:39 +0200

    simpler code quality

--- src/CodeQuality/ComplicatedLoopsInCodeReview.php
+++ src/CodeQuality/ComplicatedLoopsInCodeReview.php
@@ -5,20 +5,6 @@ namespace App\CodeQuality;
 // run: vendor/bin/rector process src/CodeQuality/ComplicatedLoopsInCodeReview.php --set code-quality -n
 final class ComplicatedLoopsInCodeReview
 {
-    public function getDefalutValue($value)
-    {
-        return $value === null ? 10 : $value;
-    }
-
-    public function isCourseFull(array $visitors)
-    {
-        if (count($visitors)) {
-            return true;
-        }
-
-        return false;
-    }
-
     public function tooComplexArrayVerification(array $items, string $name)
     {
         foreach ($items as $item) {
